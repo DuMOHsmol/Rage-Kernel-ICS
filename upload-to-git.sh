851a500489697b4adf94359f180cfd896a86393e
DuMOHsmol <dumohsmol@yandex.ru>
2012-06-20 00:32:32 +0400

    Ver 3.1, updated recovery to DooMLoRD's ICS version, fixed usb mounting issues in recovery.

--- upload-to-git.sh
+++ upload-to-git.sh
@@ -1,3 +1,3 @@
 git add ./*
-git commit -m 'Ver 3.0, fixed ramdisks one more time.'
+git commit -m "Ver 3.1, updated recovery to DooMLoRD's ICS version, fixed usb mounting issues in recovery."
 git push -u origin master
